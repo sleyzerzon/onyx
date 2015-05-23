e21671692d4cec6786b7cfd0680c6f892438555d
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-05-23 15:16:33 +0800

    Test codeship.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/onyx-platform/onyx"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :jvm-opts ["-Xmx4g"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
